9a5876a962635efda0c70af8753b4b726431a8a3
anandadio <117131125+anandadio@users.noreply.github.com>
2023-04-10 18:00:28 +0700

    Update fungsi_terapan.py

--- fungsi_terapan.py
+++ fungsi_terapan.py
@@ -1,5 +1,30 @@
 import fungsi_dasar as fd
 
 def login():
-    # contoh aja
-    return 0
+    global username                        
+    file = open('user.csv','r')
+    baris_user = file.readlines()
+    username = input("Username: ")
+    password = input("Password: ")
+    for line in baris_user:
+        #Agar Baris pertama dalam file tidak di baca
+        if line != baris_user[0]:       
+            user = split(line)
+            if user[0] == username:
+                if user[1] == password:
+                    print(f"Selamat Datang,{username} !")
+                    return True
+                else:
+                    print("Password Salah!")
+                    return False
+    print("Username tidak terdaftar!")
+    return False
+
+# Fungsi Logout
+def logout():
+    # Status sebagai penanda apakah sudah login atau belum
+    global status
+    if status == True:
+        status = False
+    else:
+        print('Login gagal!\n Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout.')
