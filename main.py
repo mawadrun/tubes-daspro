19d3f78e9471aa8b75196165d76a5130fd7fa9f1
anandadio <117131125+anandadio@users.noreply.github.com>
2023-04-10 18:32:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import fungsi_dasar as fd
 import fungsi_terapan as ft
 
-# contoh penggunaan beberapa fungsi dasar
-string = "the quick brown fox jumps over the lazy dog"
-string = fd.strSplit(string, " ")
-print(string)
+# Program Utama
+status = False              # Penanda awal bahwa belum login
+def menu():
+    global status
+    pilihan = input()
+    if pilihan == 'login':
+        if status == False:
+            if ft.login() == True:
+                status = True
+            else:
+                status = False
+        else:
+            print(f'Login gagal!\n Anda telah login dengan username {username}, silahkan lakukan “logout” sebelum melakukan login kembali.')
+        menu()
+    elif pilihan == 'logout':
+        ft.logout()
+        menu()
+
+# Memanggil/Memulai Program
+menu()
