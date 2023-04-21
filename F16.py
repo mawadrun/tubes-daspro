cbbe7c03cac8c8b673fd87a3b7fcdbf08db05df6
Ericstevann <130165530+Ericstevann@users.noreply.github.com>
2023-04-21 21:35:31 +0700

    Update F16.py

--- F16.py
+++ F16.py
@@ -1,7 +1,7 @@
-import sys 
+
 import F14
 
-def exit(data_user):
+def exit():
   opsi = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)")
 
 # menerima input dari pengguna 
@@ -10,7 +10,7 @@ while opsi != "y" and opsi != "n" or opsi != "Y" and opsi != "N":
 
 # validasi input 
 if opsi == "y" or opsi == "Y":
-  save(data_user)
+  F14.save()
   sys.exit()
  elif opsi == "n" or opsi == "N":
   sys.exit()
