fd0c56f8e3dbb05e5fec2aa655095b2ba57cebb9
Ericstevann <130165530+Ericstevann@users.noreply.github.com>
2023-04-21 23:24:31 +0700

    Update F15.py

--- F15.py
+++ F15.py
@@ -15,6 +15,46 @@ def toCSV(filename,A):
     
 def save():
   folder=input("Masukkan nama folder:")
+  if os.path.exists('save'):
+    if os.path.exists(f"save/{folder}"):
+      pass
+    else:
+      os.makedirs(f"save/{folder}")
+  else:
+    os.makedirs('save')
+    
+  toCSV(f"save/{folder}/users.csv",namamatriks)
+  toCSV(f"save/{folder}/bahan_bangunan.csv",)
+  toCSV(f"save/{folder}/candi.csv",)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 
 
