1e2c6cdba46211a4f9b4a369a8441bb5e49ed552
Ericstevann <130165530+Ericstevann@users.noreply.github.com>
2023-04-21 23:07:48 +0700

    Update F15.py

--- F15.py
+++ F15.py
@@ -1,1 +1,15 @@
-def toCSV(
+def toCSV(filename,A):
+  x=""
+  for C in A:
+    i=0
+    for B in C:
+      if i!=0 :
+        x+=";"
+      x+=B
+      i+=1
+    x+="\n"
+  f=open(filename,"w+")
+  f.write(x)
+  f.close
+    
+
